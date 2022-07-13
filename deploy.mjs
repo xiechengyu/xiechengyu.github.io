1112a871c16c1242edb71a705692492a91c94a6f
xiechengyu <2390936921@qq.com>
2022-07-13 11:00:24 +0800

    deploy

---
+++ deploy.mjs
@@ -0,0 +1,29 @@
+await $`rm -rf leetcode`
+
+await $`git clone git@github.com:xiechengyu/leetcode.git`
+
+await $`node build.js`
+
+await $`npm run build`
+
+await $`cd dist`
+
+await $`git init`
+
+await $`git add -A`
+
+await $`git commit -m 'deploy'`
+
+await $`git push -f git@github.com:xiechengyu/xiechengyu.github.io.git master`
+
+await $`cd -`
+
+await $`git pull`
+
+await $`git add .`
+
+const time = new Date()
+
+await $`git commit -m 'update ${time}'`
+
+await $`git push origin main`
